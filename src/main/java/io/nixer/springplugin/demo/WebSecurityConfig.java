a5ab0444bee5446f0a86ba13992e1d4504036f95
Kamil Wojcik <kamil.wojcik@crosswordcybersecurity.com>
2019-11-18 14:31:08 +0100

    remove extra error handler

--- src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
+++ src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
@@ -1,18 +1,12 @@
 package io.nixer.springplugin.demo;
 
-import java.util.LinkedHashMap;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 /**
  * Very basic in-memory single user IAM.
@@ -23,8 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        final LinkedHashMap<Class<? extends AuthenticationException>, AuthenticationFailureHandler> loginFailureHandlers = new LinkedHashMap<>();
-        loginFailureHandlers.put(LockedException.class, new SimpleUrlAuthenticationFailureHandler("/login?error=LOCKED")); // TODO pass info
         httpSecurity
                 .anonymous().and()
                 .authorizeRequests()
