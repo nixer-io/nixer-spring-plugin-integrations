9f706867a9b09bacbd0103d01d5d042d2c17c624
Kamil Wojcik <kamil.wojcik@crosswordcybersecurity.com>
2019-11-26 08:54:44 +0100

    - update users

--- src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
+++ src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
@@ -45,9 +45,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private Map<String, String> users() {
         final Map<String, String> users = new HashMap<>();
-        users.put("user", "user");
-        users.put("user1", "user1");
-        users.put("user2", "user2");
+        users.put("existingUser1", "gup5A686zjnVhw+v");
+        users.put("existingUser2", "PgwWldkDIP15gZ8u");
+        users.put("existingUser3", "HaMaUiG0UyCi1Yot");
+        users.put("existingUser4", "wb7ac0Ie/79XS/9Y");
+        users.put("existingUser5", "5CLWkPL5bmpGUDBg");
+        users.put("existingUser6", "ZyS67ePRz0VdKhyd");
+        users.put("existingUser7", "lTqm4co9CdDsylja");
+        users.put("existingUser8", "rEN0C+L4RNQBxKAJ");
+        users.put("existingUser9", "RYvlC6PCyjb/wfGT");
+        users.put("existingUser10", "rDRz3gXbeY0+nGQF");
+        users.put("existingUser11", "5BUluVChX1TDhIGQ");
+        users.put("existingUser12", "KfpE50i6Y/i9gup3");
+        users.put("existingUser13", "Tx9EQhFlbVBGqlnQ");
+        users.put("existingUser14", "8tC19h/F+Rnl6d5N");
+        users.put("existingUser15", "sRUZcMoghW62stvS");
+        users.put("existingUser16", "uxMKMbR4aURGjJda");
+        users.put("existingUser17", "Tx9lTtPDHK0epzp0");
+        users.put("existingUser18", "qMkDXC/Wiy1I2Mtn");
+        users.put("existingUser19", "4bDCOniyko6SgCse");
+        users.put("existingUser20", "C0wKe9kmM1IltT75");
+        users.put("leakedUser21", "C0wKe9kmM1IltT75");
+
         return users;
     }
 
