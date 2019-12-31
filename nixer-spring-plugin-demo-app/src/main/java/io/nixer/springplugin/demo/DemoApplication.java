be18c3b0650ccde155a367e5f6235607a1554c8a
Grzegorz Ćwiak <gcwiak@users.noreply.github.com>
2019-12-31 10:36:25 +0100

    Revert "Revert "How to apply nixer-spring-plugin to simple web-app""

--- nixer-spring-plugin-demo-app/src/main/java/io/nixer/springplugin/demo/DemoApplication.java
+++ nixer-spring-plugin-demo-app/src/main/java/io/nixer/springplugin/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package io.nixer.springplugin.demo;
 
+import io.nixer.nixerplugin.core.detection.events.log.EventLogConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EventLogConfiguration.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
--- nixer-spring-plugin-demo-app/src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
+++ nixer-spring-plugin-demo-app/src/main/java/io/nixer/springplugin/demo/WebSecurityConfig.java
@@ -3,6 +3,11 @@ package io.nixer.springplugin.demo;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.nixer.nixerplugin.captcha.config.CaptchaConfigurer;
+import io.nixer.nixerplugin.captcha.security.CaptchaChecker;
+import io.nixer.nixerplugin.core.detection.filter.FilterConfiguration;
+import io.nixer.nixerplugin.core.detection.filter.behavior.Conditions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +19,17 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.RequestContextFilter;
 
+import static io.nixer.nixerplugin.core.detection.filter.behavior.Behaviors.*;
+
 /**
  * Very basic in-memory single user IAM.
  */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CaptchaChecker captchaChecker;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -43,6 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         var configurer = auth.inMemoryAuthentication().passwordEncoder(encoder);
 
         users().forEach((user, pass) -> configurer.withUser(user).password(encoder.encode(pass)).roles("USER"));
+
+        configurer.withObjectPostProcessor(new CaptchaConfigurer(captchaChecker));
     }
 
     @Bean
@@ -156,4 +168,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return users;
     }
 
+    /**
+     * Configures rules. Rules define what happens at what conditions.
+     */
+    @Bean
+    public FilterConfiguration.BehaviorProviderConfigurer behaviorConfigurer() {
+        return builder -> builder
+                .rule("blacklistedIp")  // we want to hide fact that request was blocked. So pretending regular login error.
+                .when(Conditions::isBlacklistedIp)
+                .then(CAPTCHA)
+                .buildRule()
+                .rule("ipLoginOverThreshold")
+                .when(Conditions::isIpLoginOverThreshold)
+                .then(CAPTCHA)
+                .buildRule()
+                .rule("userAgentLoginOverThreshold")
+                .when(Conditions::isUserAgentLoginOverThreshold)
+                .then(CAPTCHA)
+                .buildRule()
+                .rule("usernameLoginOverThreshold")
+                .when(Conditions::isUsernameLoginOverThreshold)
+                .then(CAPTCHA)
+                .buildRule()
+                .rule("credentialStuffingActive")
+                .when(Conditions::isGlobalCredentialStuffing)
+                .then(CAPTCHA)
+                .buildRule();
+    }
 }
