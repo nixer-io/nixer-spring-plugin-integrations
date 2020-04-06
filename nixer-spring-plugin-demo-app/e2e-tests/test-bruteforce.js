3df5e30753a82727d0bb0b3bed2adb05dd3615de
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-04-06 15:08:41 +0200

    chore: Delete e2e-tests as it's contents have already been moved to nixer-testing repository.

--- nixer-spring-plugin-demo-app/e2e-tests/test-bruteforce.js
+++
@@ -1,8 +0,0 @@
-var runner = require('./test-runner');
-var argv = require('yargs').argv;
-
-runner.run({
-    name: 'test-bruteforce',
-    dataFile: './test-bruteforce.data.csv',
-    reporter: argv.reporter
-});
--- nixer-spring-plugin-demo-app/e2e-tests/test-cs-user-agent.js
+++
@@ -1,8 +0,0 @@
-var runner = require('./test-runner');
-var argv = require('yargs').argv;
-
-runner.run({
-    name: 'test-cs-user-agent',
-    dataFile: './test-cs.data.csv',
-    reporter: argv.reporter
-});
