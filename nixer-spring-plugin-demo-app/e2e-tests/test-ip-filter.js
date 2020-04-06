14214141d26ad56dd4d4204fa0e7801ab478a0cb
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-04-06 15:03:49 +0200

    chore: Delete e2e-tests as it's contents have already been moved to nixer-testing repository.

--- nixer-spring-plugin-demo-app/e2e-tests/test-ip-filter.js
+++
@@ -1,8 +0,0 @@
-var runner = require('./test-runner');
-var argv = require('yargs').argv;
-
-runner.run({
-    name: 'test-ip-filter',
-    dataFile: './test-ip-filter.data.csv',
-    reporter: argv.reporter
-});
